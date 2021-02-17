f7536feb2036c1bda3365752235b4633498313de
DMC Naruto <51503516+ashutosh1615@users.noreply.github.com>
2021-02-17 17:42:43 +0530

    Add files via upload

---
+++ Utility.py
@@ -0,0 +1,21 @@
+import math
+def add(operator,x,y):
+        return x+y
+def subtract(operator,x,y):
+        return x-y
+def divide(operator,x,y):
+        return x/y
+def multiply(operator,x,y):
+        return x*y
+def power(operator,x,y):
+        return x**y
+def card(start,end):
+    exp=0
+    for i in range(start,end):
+        exp = exp+math.floor(10*(i**1.5))
+    silver = math.ceil(exp/200)
+    gold= math.ceil(exp/250)
+    platinum = math.ceil(exp/300)
+    return (silver,gold,platinum)        
+
+               
